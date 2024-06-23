a667a28da04072a105f948a5aa6d74ebb8f4f770
matteobergantin <matteobergantin161@gmail.com>
2024-06-23 15:15:18 +0200

    Added variance in NPC spawns && added exit elevator animation

--- main.py
+++ main.py
@@ -62,19 +62,21 @@ class Elevator(pygame.sprite.Sprite):
 
 # Person sprite
 class Person(pygame.sprite.Sprite):
-    IMAGES = {
-        'WALKING': ['guy/guy_walking_1.png', 'guy/guy_walking_2.png'],
-        'BASE': 'guy/guy_walking_0.png'
-    }
 
     def __init__(self, x, y, weight, priority, target_floor):
         super().__init__()
-        img = pygame.image.load(Person.IMAGES['BASE'])
+        all_colors = [ 'blue', 'yellow', 'red' ]
+        random_chosen_color = all_colors[random.randint(0, len(all_colors)-1)]
+        self.IMAGES = {
+            'WALKING': [f'guy/{random_chosen_color}_guy_walking_1.png', f'guy/{random_chosen_color}_guy_walking_2.png'],
+            'BASE': f'guy/{random_chosen_color}_guy_walking_0.png'
+        }
+        img = pygame.image.load(self.IMAGES['BASE'])
         img = pygame.transform.scale(img, (int(SCREEN_WIDTH * 0.05), int(SCREEN_HEIGHT * 0.05)))
         self.base_image = img
         self.image = img
         self.walking_animation_imgs = []
-        for img_path in Person.IMAGES['WALKING']:
+        for img_path in self.IMAGES['WALKING']:
             img = pygame.image.load(img_path)
             img = pygame.transform.scale(img, (int(SCREEN_WIDTH * 0.05), int(SCREEN_HEIGHT * 0.05)))
             self.walking_animation_imgs.append(img)
@@ -91,6 +93,7 @@ class Person(pygame.sprite.Sprite):
         self.run_walking_animation = False
         self.steps = 50
         self.current_step = 0
+        self.is_running_backwards = False
 
     def enter_elevator(self, elevator):
         if not self.in_elevator and (elevator.current_weight + self.weight <= elevator.max_weight):
@@ -101,23 +104,37 @@ class Person(pygame.sprite.Sprite):
             self.target_x = elevator.rect.x + 10 + len(elevator.passengers) * 20
             self.current_step = 0
             self.step_size = abs(self.rect.x - self.target_x) / self.steps
+            self.is_running_backwards = False
     
-    def step_animation(self):
-        if not self.run_walking_animation:
-            self.image = self.base_image
-            return
+    def step_walking_animation(self):
         self.image = self.walking_animation_imgs[self.current_step % len(self.walking_animation_imgs)]
+        if self.is_running_backwards:
+            self.image = pygame.transform.flip(self.image, 1, 0)
+
         self.current_step += 1
         self.rect.x += self.step_size
-        if self.current_step == self.steps:
+        if self.current_step == self.steps and not self.is_running_backwards:
             self.run_walking_animation = False
+            self.image = self.base_image
+            self.current_step = 0
+            self.image = pygame.transform.flip(self.image, 1, 0)
 
-    def exit_elevator(self, elevator):
+    def step_all_animations(self):
+        if self.run_walking_animation:
+            self.step_walking_animation()
+
+    def exit_elevator(self, elevator, floor):
         if self.in_elevator:
-            self.rect.x = 20 + len([p for p in people if not p.in_elevator]) * 30
+            #self.rect.x = 20 + len([p for p in people if not p.in_elevator]) * 30
+            self.rect.y = floor
             elevator.current_weight -= self.weight
             self.in_elevator = False
-    
+            self.target_x = -1
+            self.run_walking_animation = True
+            self.current_step = 0
+            self.step_size = -10
+            self.is_running_backwards = True
+
     def __str__(self):
         return f"Person(target_floor={self.target_floor}), in_elevator={self.in_elevator})"
 
@@ -171,10 +188,8 @@ while running:
                 for person in elevator.passengers:
                     if person.target_floor == current_floor:
                         elevator.passengers.remove(person)
-                        person.exit_elevator(elevator)
-                        person.rect.y = floors[current_floor]  # Set the person to the current floor
-
-
+                        person.exit_elevator(elevator, floors[current_floor])
+                        #person.rect.y = floors[current_floor]  # Set the person to the current floor
 
     # Move the elevator
     if moving and target_floor is not None:
@@ -195,6 +210,6 @@ while running:
     clock.tick(FPS)
 
     for p in people:
-        p.step_animation()
+        p.step_all_animations()
     
 pygame.quit()
